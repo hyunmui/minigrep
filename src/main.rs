29dcd2360f33170118d3f22d8de105c60b61443b
Hyeonmin Seon <hmsun.es@esgroup.net>
2022-01-16 20:20:16 +0900

    12.3.1

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,25 @@
-use std::env;
+use std::{env, fs};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let filename = &args[2];
+    let config = parse_config(&args);
 
-    println!("search word: {}", query);
-    println!("target file name: {}", filename);
+    println!("search word: {}", config.query);
+    println!("target file name: {}", config.filename);
+
+    let contents = fs::read_to_string(config.filename).expect("Can't read this file");
+
+    println!("File Content: \n{}", contents);
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
+
+    Config { query, filename }
 }
