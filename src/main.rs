16ba2c38ca8fd3f10d294c1b3f924b1483c88d0f
Hyeonmin Seon <hmsun.es@esgroup.net>
2022-01-16 20:29:46 +0900

    기본 형태 완성

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,20 @@
-use std::{env, fs};
+use std::{env, process};
+
+use minigrep::run;
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = parse_config(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        print!("An error occurred while parsing the argument: {}", err);
+        process::exit(1);
+    });
 
     println!("search word: {}", config.query);
     println!("target file name: {}", config.filename);
 
-    let contents = fs::read_to_string(config.filename).expect("Can't read this file");
-
-    println!("File Content: \n{}", contents);
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let filename = args[2].clone();
-
-    Config { query, filename }
+    if let Err(e) = run(config) {
+        println!("Application Error: {}", e);
+        process::exit(1);
+    }
 }
